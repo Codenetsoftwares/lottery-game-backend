831613f05ba8e7570db1e01129eb2d2fea10b64b
masudcns <masudur07cns@gmail.com>
2024-09-13 17:58:21 +0530

    fix

--- middlewares/colorgameAuth.js
+++ middlewares/colorgameAuth.js
@@ -6,20 +6,16 @@ import { apiResponseErr } from '../utills/response.js';
 dotenv.config();
 
 export const authenticateUser = (req, res, next) => {
-    console.log("..............");
     const authHeader = req.headers['authorization'];
     if (!authHeader) {
         return apiResponseErr(null, false, statusCode.unauthorize, 'Access denied. No token provided.', res)
     }
-    console.log("authHeader", authHeader);
     const token = authHeader.split(' ')[1];
-    console.log("token", token);
 
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
         if (err) {
             return apiResponseErr(null, false, statusCode.unauthorize, 'Unauthorized Access', res)
         }
-        console.log("decoded", decoded)
 
         const role = string.User
         const userRole = decoded.roles;
