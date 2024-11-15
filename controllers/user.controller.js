6bbf118f5621a9d95cb92c2b10360127091386fa
riktadeyy <rikta.dey.cns@gmail.com>
2024-11-15 16:14:25 +0530

    modify search api

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -45,41 +45,52 @@ export const getAllMarkets = async (req, res) => {
   }
 };
 
-export const searchTickets = async ({ group, series, number, sem, marketId }) => {
+export const searchTickets = async ({ group, series, number, sem }) => {
   try {
     const today = new Date();
     today.setHours(0, 0, 0, 0);
 
-    const query = {
-      group_start: { [Op.lte]: group },
-      group_end: { [Op.gte]: group },
-      series_start: { [Op.lte]: series },
-      series_end: { [Op.gte]: series },
-      number_start: { [Op.lte]: number },
-      number_end: { [Op.gte]: number },
-      createdAt: { [Op.gte]: today },
-      ...(marketId && { marketId }), // Add marketId condition if it is provided
-    };
-
     const result = await TicketRange.findOne({
-      where: query,
+      where: {
+        group_start: { [Op.lte]: group }, // group_start <= user group
+        group_end: { [Op.gte]: group }, // group_end >= user group
+        series_start: { [Op.lte]: series }, // series_start <= user series
+        series_end: { [Op.gte]: series }, // series_end >= user series
+        number_start: { [Op.lte]: number }, // number_start <= user number
+        number_end: { [Op.gte]: number }, // number_end >= user number
+        createdAt: { [Op.gte]: today },
+      },
+    });
+
+    const createRange = await UserRange.create({
+      generateId: uuidv4(),
+      group,
+      series,
+      number,
+      sem,
     });
 
     if (result) {
-      const ticketService = new TicketService(group, series, number, sem);
+      const ticketService = new TicketService(
+        group,
+        series,
+        number.toString(),
+        sem
+      );
 
       const tickets = ticketService.list();
       const price = ticketService.calculatePrice();
-      return { tickets, price, sem  };
+      return { tickets, price, sem, generateId: createRange.generateId };
     } else {
       return {
         data: [],
         success: true,
         successCode: 200,
-        message: 'No tickets available in the given range or market.',
+        message: "No tickets available in the given range.",
       };
     }
   } catch (error) {
+    console.error("Error saving ticket range:", error);
     return new CustomError(error.message, null, statusCode.internalServerError);
   }
 };
