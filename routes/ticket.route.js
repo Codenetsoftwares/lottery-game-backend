c28b49ecb20512e9693eb702ada2699e665327d4
masudcns <masudur07cns@gmail.com>
2024-11-15 17:43:13 +0530

    add validation generate- ticket Api

--- routes/ticket.route.js
+++ routes/ticket.route.js
@@ -1,8 +1,9 @@
 import { string } from '../constructor/string.js';
 import { geTicketRange, saveTicketRange } from '../controllers/ticket.controller.js';
 import { authorize } from '../middlewares/auth.js';
+import { createTicketValidation } from '../utils/commonSchema.js';
 
 export const ticketRoute = (app) => {
-  app.post('/api/generate-ticket', authorize([string.Admin]), saveTicketRange);
+  app.post('/api/generate-ticket',createTicketValidation, authorize([string.Admin]), saveTicketRange);
   app.get('/api/get-range', geTicketRange);
 };
