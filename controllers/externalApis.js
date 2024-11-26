e082bb1573edfd74529a7bc724c9834d031213df
riktadeyy <rikta.dey.cns@gmail.com>
2024-11-26 12:48:43 +0530

    modify the ber history and add prod env

--- controllers/externalApis.js
+++ controllers/externalApis.js
@@ -5,29 +5,40 @@ import { statusCode } from "../utils/statusCodes.js";
 
 export const getLotteryBetHistory = async (req, res) => {
     try {
-        const { userId } = req.body
-        const purchaseLotteries = await PurchaseLottery.findAll({ where: { userId } });
+        const { userId, userName } = req.body;
+
+        console.log("userId, userName ", userId, userName)
+
+        const queryConditions = {};
+        if (userId) queryConditions.userId = userId;
+        if (userName) queryConditions.userName = userName;
+
+        const purchaseLotteries = await PurchaseLottery.findAll({ where: queryConditions });
+
 
         if (purchaseLotteries.length === 0) {
             return apiResponseSuccess([], true, statusCode.success, "No bet history found", res);
         }
 
-        const betHistory = purchaseLotteries.map(purchase => {
-            const { group, series, number, sem } = purchase;
-            const ticketService = new TicketService(group, series, number, sem);
+        const betHistory = await Promise.all(
+            purchaseLotteries.map(async (purchase) => {
+                const { group, series, number, sem, marketId } = purchase;
+                const ticketService = new TicketService();
 
-            const tickets = ticketService.list();
+                const tickets = await ticketService.list(group, series, number, sem, marketId);
 
-            return {
-                gameName: "Lottery",
-                marketName: purchase.marketName,
-                marketId: purchase.marketId,
-                amount: purchase.lotteryPrice,
-                ticketPrice: purchase.price,
-                tickets,
-                sem
-            };
-        });
+                return {
+                    gameName: "Lottery",
+                    marketName: purchase.marketName,
+                    marketId: purchase.marketId,
+                    amount: purchase.lotteryPrice,
+                    ticketPrice: purchase.price,
+                    tickets,
+                    sem,
+                };
+            })
+        );
+
         return apiResponseSuccess(betHistory, true, statusCode.success, 'success', res);
     } catch (error) {
         return apiResponseErr(null, false, statusCode.internalServerError, error.message, res);
--- routes/externalApis.route.js
+++ routes/externalApis.route.js
@@ -1,9 +1,6 @@
 import { getLotteryBetHistory } from "../controllers/externalApis.js";
-import { authenticateUser } from "../middlewares/colorgameAuth.js";
-import { validateGetLotteryBetHistory } from "../utils/commonSchema.js";
-import customErrorHandler from "../utils/customErrorHandler.js";
 
 export const ExternalApiModule = (app) => {
-    app.post('/api/lottery-external-bet-history', validateGetLotteryBetHistory, customErrorHandler, authenticateUser, getLotteryBetHistory);
+    app.post('/api/lottery-external-bet-history', getLotteryBetHistory);
 
 }
