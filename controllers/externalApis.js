25b127aa17102d0c61c1c2d2dbec1728a950e52b
riktadeyy <rikta.dey.cns@gmail.com>
2024-11-27 10:50:51 +0530

    marketAnalysis of lottery

--- controllers/externalApis.js
+++ controllers/externalApis.js
@@ -1,3 +1,4 @@
+import { Op } from "sequelize";
 import { TicketService } from "../constructor/ticketService.js";
 import PurchaseLottery from "../models/purchase.model.js";
 import { apiResponseErr, apiResponseSuccess } from "../utils/response.js";
@@ -40,6 +41,57 @@ export const getLotteryBetHistory = async (req, res) => {
         );
 
         return apiResponseSuccess(betHistory, true, statusCode.success, 'success', res);
+    } catch (error) {
+        return apiResponseErr(null, false, statusCode.internalServerError, error.message, res);
+    }
+};
+
+
+export const lotteryMarketAnalysis = async (req, res) => {
+    try {
+        const { marketId } = req.params;
+
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        const purchaseLotteries = await PurchaseLottery.findAll({
+            where: {
+                marketId,
+                createdAt: {
+                    [Op.gte]: today,
+                },
+            }
+        });
+
+        if (purchaseLotteries.length === 0) {
+            return apiResponseSuccess([], true, statusCode.success, "No bet history found", res);
+        }
+
+        const totalAmount = purchaseLotteries.reduce((sum, purchase) => sum + purchase.lotteryPrice, 0);
+
+        const commonData = {
+            gameName: "Lottery",
+            marketName: purchaseLotteries[0].marketName,
+            marketId: purchaseLotteries[0].marketId,
+            amount: totalAmount,
+        };
+
+        const ticketsData = await Promise.all(
+            purchaseLotteries.map(async (purchase) => {
+                const { group, series, number, sem, marketId } = purchase;
+                const ticketService = new TicketService();
+                const tickets = await ticketService.list(group, series, number, sem, marketId);
+
+                return { tickets, sem };
+            })
+        );
+
+        const response = {
+            ...commonData,
+            details: ticketsData
+        };
+
+        return apiResponseSuccess(response, true, statusCode.success, 'success', res);
     } catch (error) {
         return apiResponseErr(null, false, statusCode.internalServerError, error.message, res);
     }
--- routes/externalApis.route.js
+++ routes/externalApis.route.js
@@ -1,6 +1,9 @@
-import { getLotteryBetHistory } from "../controllers/externalApis.js";
+import { getLotteryBetHistory, lotteryMarketAnalysis } from "../controllers/externalApis.js";
 
 export const ExternalApiModule = (app) => {
     app.post('/api/lottery-external-bet-history', getLotteryBetHistory);
+
+    app.get('/api/lottery-external-marketAnalysis/:marketId', lotteryMarketAnalysis);
+
 
 }
