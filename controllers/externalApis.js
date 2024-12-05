194b4273267a8a4152b463f4c078dcb20dead3b3
riktadeyy <rikta.dey.cns@gmail.com>
2024-12-05 12:09:25 +0530

    update market analysis

--- controllers/externalApis.js
+++ controllers/externalApis.js
@@ -142,98 +142,99 @@ export const getLotteryBetHistory = async (req, res) => {
 };
 
 export const lotteryMarketAnalysis = async (req, res) => {
-    try {
-        const { marketId } = req.params;
+  try {
+    const { marketId } = req.params;
 
-        const today = new Date();
-        today.setHours(0, 0, 0, 0);
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
 
-        const purchaseLotteries = await PurchaseLottery.findAll({
-            where: {
-                marketId,
-                createdAt: {
-                    [Op.gte]: today,
-                },
-            }
-        });
+    const purchaseLotteries = await PurchaseLottery.findAll({
+      where: {
+        marketId,
+        date: {
+          [Op.gte]: today,
+        },
+        resultAnnouncement: false
+      }
+    });
 
-        if (purchaseLotteries.length === 0) {
-            return apiResponseSuccess([], true, statusCode.success, "No bet history found", res);
-        }
-
-        const totalAmount = purchaseLotteries.reduce((sum, purchase) => sum + purchase.lotteryPrice, 0);
-
-        const commonData = {
-            gameName: "Lottery",
-            marketName: purchaseLotteries[0].marketName,
-            marketId: purchaseLotteries[0].marketId,
-            amount: totalAmount,
-        };
-
-        const ticketsData = await Promise.all(
-            purchaseLotteries.map(async (purchase) => {
-                const { group, series, number, sem, marketId } = purchase;
-                const ticketService = new TicketService();
-                const tickets = await ticketService.list(group, series, number, sem, marketId);
-
-                return { tickets, sem };
-            })
-        );
-
-        const response = {
-            ...commonData,
-            details: ticketsData
-        };
-
-        return apiResponseSuccess(response, true, statusCode.success, 'success', res);
-    } catch (error) {
-        return apiResponseErr(null, false, statusCode.internalServerError, error.message, res);
+    if (purchaseLotteries.length === 0) {
+      return apiResponseSuccess([], true, statusCode.success, "No bet history found", res);
     }
+
+    const totalAmount = purchaseLotteries.reduce((sum, purchase) => sum + purchase.lotteryPrice, 0);
+
+    const commonData = {
+      gameName: "Lottery",
+      marketName: purchaseLotteries[0].marketName,
+      marketId: purchaseLotteries[0].marketId,
+      amount: totalAmount,
+    };
+
+    const ticketsData = await Promise.all(
+      purchaseLotteries.map(async (purchase) => {
+        const { group, series, number, sem, marketId } = purchase;
+        const ticketService = new TicketService();
+        const tickets = await ticketService.list(group, series, number, sem, marketId);
+
+        return { tickets, sem };
+      })
+    );
+
+    const response = {
+      ...commonData,
+      details: ticketsData
+    };
+
+    return apiResponseSuccess(response, true, statusCode.success, 'success', res);
+  } catch (error) {
+    return apiResponseErr(null, false, statusCode.internalServerError, error.message, res);
+  }
 }
 
 export const getBetHistoryP_L = async (req, res) => {
-    try {
-        const { userId, userName } = req.body
-        const queryConditions = {}
-        if (userId) queryConditions.userId = userId;
-        if (userName) queryConditions.userName = userName;
+  try {
+    const { userId, userName } = req.body
+    const queryConditions = {}
+    if (userId) queryConditions.userId = userId;
+    if (userName) queryConditions.userName = userName;
 
-        const purchaseLotteries = await PurchaseLottery.findAll({
-            where: queryConditions,
-        });
+    const purchaseLotteries = await PurchaseLottery.findAll({
+      where: queryConditions,
+    });
 
-        if (purchaseLotteries.length === 0) {
-            return apiResponseSuccess([], true, statusCode.success, "No bet history found", res);
-        }
+    if (purchaseLotteries.length === 0) {
+      return apiResponseSuccess([], true, statusCode.success, "No bet history found", res);
+    }
 
-        const betHistory = await Promise.all(
-            purchaseLotteries.map(async (purchase) => {
-                const { group, series, number, sem, marketId } = purchase;
-                const ticketService = new TicketService();
+    const betHistory = await Promise.all(
+      purchaseLotteries.map(async (purchase) => {
+        const { group, series, number, sem, marketId } = purchase;
+        const ticketService = new TicketService();
 
-                const tickets = await ticketService.list(
-                    group,
-                    series,
-                    number,
-                    sem,
-                    marketId
-                );
-
-                return {
-                    userName: purchase.userName,
-                    gameName: "Lottery",
-                    marketName: purchase.marketName,
-                    marketId: purchase.marketId,
-                    amount: purchase.lotteryPrice,
-                    ticketPrice: purchase.price,
-                    tickets,
-                    sem,
-                };
-            })
+        const tickets = await ticketService.list(
+          group,
+          series,
+          number,
+          sem,
+          marketId
         );
 
-        return apiResponseSuccess(betHistory, true, statusCode.success, 'success', res);
-    } catch (error) {
-        return apiResponseErr(null, false, statusCode.internalServerError, error.message, res);
-    }
+        return {
+          userName: purchase.userName,
+          gameName: "Lottery",
+          marketName: purchase.marketName,
+          marketId: purchase.marketId,
+          amount: purchase.lotteryPrice,
+          ticketPrice: purchase.price,
+          tickets,
+          sem,
+        };
+      })
+    );
+
+    return apiResponseSuccess(betHistory, true, statusCode.success, 'success', res);
+  } catch (error) {
+    return apiResponseErr(null, false, statusCode.internalServerError, error.message, res);
+  }
 };
